9afa26ced7fbdb2f8305f3b9d6055dc74294924d
Pinchas <139820013+pmk248@users.noreply.github.com>
2024-09-18 17:26:15 +0300

    added class for card generation assistance

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 "use strict";
-//---------- DATA STRUCTURES: ----------//
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,14 +8,52 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+//---------- BOOTSTRAP SVGs: ----------//
+let fullSquare = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square-fill" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2z"/>
+</svg>`;
 var Status;
 (function (Status) {
     Status[Status["Available"] = 0] = "Available";
-    Status[Status["Repairs"] = 1] = "Repairs";
+    Status[Status["Undergoing Repair"] = 1] = "Undergoing Repair";
     Status[Status["Unavailable"] = 2] = "Unavailable";
 })(Status || (Status = {}));
 ;
+class ScooterCard {
+    constructor(element, scooterData) {
+        this.element = element;
+        this.scooterData = scooterData;
+        this.renderCards();
+    }
+    ;
+    renderCards() {
+        this.element.classList.add('card');
+        this.element.innerHTML = `
+            <h2>${this.scooterData.model} </h2>
+            <image href="${this.scooterData.imageUrl}">
+            <p> Color: <span color="${this.scooterData.color}">${fullSquare}</span></p>
+            <p> Status: ${this.scooterData.status} </p>
+            <p> Battery: ${this.scooterData.batteryLevel}% </p>
+            <p class="edit-delete-container"><button class="edit-button"> Edit <button>
+            <button class="delete-button"> Delete </button></p>
+            `;
+        this.addEventListeners();
+    }
+    ;
+    addEventListeners() {
+        var _a, _b;
+        (_a = this.element.querySelector('.delete-button')) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+            removeScooter(this.scooterData.id);
+        });
+        (_b = this.element.querySelector('.edit-button')) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => {
+            openEditForm(this.scooterData.id);
+        });
+    }
+    ;
+}
+;
 //---------- API FUNCTIONS: ----------//
+const BASE_URL = "https://66eabf1755ad32cda47a3afc.mockapi.io/api/storage/scooters";
 function getScooter(id) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -49,7 +86,6 @@ function getAllScooters() {
     });
 }
 ;
-const BASE_URL = "https://66ea9b7555ad32cda479a15b.mockapi.io/api";
 function addScooter(newScooter) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -63,7 +99,6 @@ function addScooter(newScooter) {
             if (!response.ok) {
                 throw new Error("Scooter uploading failed!");
             }
-            alert("Scooter uploaded successfully!");
         }
         catch (error) {
             throw error;
@@ -83,8 +118,6 @@ function removeScooter(id) {
             if (!response.ok) {
                 throw new Error("Scooter fetching failed!");
             }
-            let scooter = yield response.json();
-            return scooter;
         }
         catch (error) {
             throw error;
@@ -103,7 +136,7 @@ function editScooter(id, edittedScooter) {
                 body: JSON.stringify(edittedScooter)
             });
             if (!response.ok) {
-                throw new Error("Scooter fetching failed!");
+                throw new Error("Scooter updating failed!");
             }
         }
         catch (error) {
@@ -112,3 +145,23 @@ function editScooter(id, edittedScooter) {
     });
 }
 ;
+//---------- DOM FUNCTIONS: ----------//
+document.addEventListener("DOMContentLoaded", () => {
+    renderCards();
+});
+const FORM = document.getElementById('create-form');
+const CONTAINER = document.getElementById('card-container');
+function renderCards() {
+    const allScooters = getAllScooters();
+}
+//---------- TESTING AREA: ----------//
+// let xiaomi: Scooter = {model: "xiaomi 340x", batteryLevel: 80, imageUrl: "https://placeholder/example324.jpeg", color: "#000000", status: 0};
+// let ninebot: Scooter = {model: "ninebot 4", batteryLevel: 70, imageUrl: "https://placeholder/example333.jpeg", color: "#101010", status: 1};
+//addScooter(xiaomi).then(() => console.log("Scooter added successfully")).catch(error => console.error(error));
+//addScooter(ninebot).then(() => console.log("Scooter added successfully")).catch(error => console.error(error));
+//getScooter("4").then(result => console.log(result)).catch(error => console.error(error));
+//getAllScooters().then(result => console.log(result)).catch(error => console.error(error));
+//editScooter("3", {model: "NOT xiaomi 340x", batteryLevel: 80, imageUrl: "https://placeholder/example324.jpeg", color: "#000000", status: 0}).then(() => console.log("Scooter edited successfully")).catch(error => console.error(error));
+//getAllScooters().then(result => console.log(result)).catch(error => console.error(error));
+//removeScooter("4").then(() => console.log("Scooter removed successfully")).catch(error => console.error(error));
+//getAllScooters().then(result => console.log("all scooters:\n", result)).catch(error => console.error(error));
--- script.ts
+++ script.ts
@@ -1,5 +1,9 @@
-//---------- DATA STRUCTURES: ----------//
+//---------- BOOTSTRAP SVGs: ----------//
+let fullSquare: string = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-square-fill" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2z"/>
+</svg>`
 
+//---------- DATA STRUCTURES: ----------//
 interface Scooter {
     serialNumber?: string;
     model: string;
@@ -10,11 +14,44 @@ interface Scooter {
 }
 enum Status {
     "Available",
-    "Repairs",
+    "Undergoing Repair",
     "Unavailable"
 };
 
+class ScooterCard {
+    element: HTMLElement;
+    scooterData: any;
+    constructor(element: HTMLElement, scooterData: any) {
+        this.element = element;
+        this.scooterData = scooterData;
+        this.renderCards();
+    };
+    renderCards(): void {
+        this.element.classList.add('card');
+        this.element.innerHTML = `
+            <h2>${this.scooterData.model} </h2>
+            <image href="${this.scooterData.imageUrl}">
+            <p> Color: <span color="${this.scooterData.color}">${fullSquare}</span></p>
+            <p> Status: ${this.scooterData.status} </p>
+            <p> Battery: ${this.scooterData.batteryLevel}% </p>
+            <p class="edit-delete-container"><button class="edit-button"> Edit <button>
+            <button class="delete-button"> Delete </button></p>
+            `;
+        this.addEventListeners();
+    };
+    addEventListeners(): void {
+        this.element.querySelector('.delete-button')?.addEventListener("click", () => {
+            removeScooter(this.scooterData.id);
+        });
+        this.element.querySelector('.edit-button')?.addEventListener("click", () => {
+            openEditForm(this.scooterData.id);
+        });
+    };
+};
+
 //---------- API FUNCTIONS: ----------//
+const BASE_URL = "https://66eabf1755ad32cda47a3afc.mockapi.io/api/storage/scooters";
+
 async function getScooter(id: string): Promise<Scooter> {
     try {
         const response = await fetch(`${BASE_URL}/${id}`);
@@ -27,7 +64,6 @@ async function getScooter(id: string): Promise<Scooter> {
         throw error;
     } 
 };
-
 async function getAllScooters(): Promise<Array<Scooter>> {
     try {
         const response = await fetch(BASE_URL);
@@ -41,8 +77,6 @@ async function getAllScooters(): Promise<Array<Scooter>> {
     } 
 };
 
-const BASE_URL = "https://66ea9b7555ad32cda479a15b.mockapi.io/api";
-
 async function addScooter(newScooter: Scooter): Promise<void> {
     try {
         const response = await fetch(BASE_URL, {
@@ -55,7 +89,6 @@ async function addScooter(newScooter: Scooter): Promise<void> {
         if (!response.ok) {
             throw new Error("Scooter uploading failed!");
         }
-        alert("Scooter uploaded successfully!");
 
     } catch (error) {
         throw error;
@@ -74,8 +107,6 @@ async function removeScooter(id: string): Promise<void> {
         if (!response.ok) {
             throw new Error("Scooter fetching failed!");
         }
-        let scooter = await response.json();
-        return scooter;
     } catch (error) {
         throw error;
     } 
@@ -91,9 +122,42 @@ async function editScooter(id: string, edittedScooter: Scooter): Promise<void> {
             body: JSON.stringify(edittedScooter)
         });
         if (!response.ok) {
-            throw new Error("Scooter fetching failed!");
+            throw new Error("Scooter updating failed!");
         }
     } catch (error) {
         throw error;
     } 
 };
+
+//---------- DOM FUNCTIONS: ----------//
+document.addEventListener("DOMContentLoaded", () => {
+    renderCards();
+});
+
+const FORM = document.getElementById('create-form');
+const CONTAINER = document.getElementById('card-container');
+
+
+function renderCards(): void {
+    const allScooters = getAllScooters();
+}
+
+
+
+
+
+
+
+
+//---------- TESTING AREA: ----------//
+
+// let xiaomi: Scooter = {model: "xiaomi 340x", batteryLevel: 80, imageUrl: "https://placeholder/example324.jpeg", color: "#000000", status: 0};
+// let ninebot: Scooter = {model: "ninebot 4", batteryLevel: 70, imageUrl: "https://placeholder/example333.jpeg", color: "#101010", status: 1};
+//addScooter(xiaomi).then(() => console.log("Scooter added successfully")).catch(error => console.error(error));
+//addScooter(ninebot).then(() => console.log("Scooter added successfully")).catch(error => console.error(error));
+//getScooter("4").then(result => console.log(result)).catch(error => console.error(error));
+//getAllScooters().then(result => console.log(result)).catch(error => console.error(error));
+//editScooter("3", {model: "NOT xiaomi 340x", batteryLevel: 80, imageUrl: "https://placeholder/example324.jpeg", color: "#000000", status: 0}).then(() => console.log("Scooter edited successfully")).catch(error => console.error(error));
+//getAllScooters().then(result => console.log(result)).catch(error => console.error(error));
+//removeScooter("4").then(() => console.log("Scooter removed successfully")).catch(error => console.error(error));
+//getAllScooters().then(result => console.log("all scooters:\n", result)).catch(error => console.error(error));
